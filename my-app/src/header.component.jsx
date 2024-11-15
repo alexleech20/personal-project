abbdb27fcf27a9d4e488eee1bd3e7fa1669d49f0
alexander.leech <alexander.leech@capita.com>
2024-11-15 16:44:52 +0000

    frame for header is down, will later revisit to add links

---
+++ my-app/src/header.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AppBar, Typography, Toolbar } from '@mui/material';
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <AppBar sx={{backgroundColor: 'inherit', boxShadow: 'none'}}>
+        <Toolbar sx={{justifyContent: 'center'}}>
+          <Typography sx={{paddingLeft: '2rem'}}>About</Typography>
+          <Typography sx={{paddingLeft: '2rem'}}>My experience</Typography>
+          <Typography sx={{paddingLeft: '2rem'}}>Contact me</Typography>
+        </Toolbar>
+      </AppBar> 
+    </React.Fragment>
+  );
+};
+
+export default Header;
